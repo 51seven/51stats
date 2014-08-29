44e411454683fdd72013cc7651de78290b738ddd
Timo Mämecke <timo@maemecke.com>
2014-08-29 16:55:18 +0200

    indention with spaces

--- modules/spotify-web-profile-scraper.js
+++ modules/spotify-web-profile-scraper.js
@@ -1,55 +1,55 @@
 var https      = require('https'),
-	when       = require('when'),
-	cheerio    = require('cheerio'),
-	async 	   = require('async');
+    when       = require('when'),
+    cheerio    = require('cheerio'),
+    async      = require('async');
 
 // HTTPS GET Request to Spotify User Profil Page
 function getPublicProfile(userid) {
-	return when.promise(function(resolve, reject, notify) {
-		https.get({ host: 'open.spotify.com', path: '/user/' + userid }, function(res) {
-			var body = '';
+    return when.promise(function(resolve, reject, notify) {
+        https.get({ host: 'open.spotify.com', path: '/user/' + userid }, function(res) {
+            var body = '';
 
-			res.on('data', function(chunk) {
-				body += chunk;
-			});
-			res.on('end', function() {
-				resolve(body);
-			});
-		}).on('error', function(err) {
-			reject(err);
-		});
-	});
+            res.on('data', function(chunk) {
+                body += chunk;
+            });
+            res.on('end', function() {
+                resolve(body);
+            });
+        }).on('error', function(err) {
+            reject(err);
+        });
+    });
 }
 
 module.exports = function(userid) {
-	return when.promise(function(resolve, reject) {
-		getPublicProfile(userid)
-			.then(function(body) {
-				var $ = cheerio.load(body),
-					track_ids = [],
-					name, profile_pic;
+    return when.promise(function(resolve, reject) {
+        getPublicProfile(userid)
+            .then(function(body) {
+                var $ = cheerio.load(body),
+                    track_ids = [],
+                    name, profile_pic;
 
-				if(body.indexOf('<h3>Top Tracks') > -1) {
-					$('li.single-track').each(function(i, elem) {
-						track_ids[i] = $(this).attr('data-id');
-					});
-				}
+                if(body.indexOf('<h3>Top Tracks') > -1) {
+                    $('li.single-track').each(function(i, elem) {
+                        track_ids[i] = $(this).attr('data-id');
+                    });
+                }
 
-				name = $('meta[property="og:title"]').attr('content');
-				profile_pic = $('#big-cover').attr('src');
+                name = $('meta[property="og:title"]').attr('content');
+                profile_pic = $('#big-cover').attr('src');
 
-				return {
-					name: name,
-					img: profile_pic,
-					top_tracks: track_ids
-				};
+                return {
+                    name: name,
+                    img: profile_pic,
+                    top_tracks: track_ids
+                };
 
-			})
-			.then(function(result) {
-				resolve(result);
-			})
-			.catch(function(err) {
-				reject(err)
-			});
-	});
+            })
+            .then(function(result) {
+                resolve(result);
+            })
+            .catch(function(err) {
+                reject(err)
+            });
+    });
 };
